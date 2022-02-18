b672655b9591e39a60d64618c896b8d718de5ee8
bunny-mack <bmcintyre.133@gmail.com>
2022-02-17 18:13:16 -0600

    Through 12.6.3

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,9 +1,17 @@
 package org.launchcode.spaday.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm(Model model){
+        model.addAttribute("title", "Add User");
+        return "/user/add.html";
+    }
 }
